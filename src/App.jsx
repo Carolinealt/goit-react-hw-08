aad8cb4c1a78bedcbbdd30ed9ef47940acc55fa6
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-07-09 17:26:03 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import { Suspense } from "react";
+import AppBar from "./components/AppBar/AppBar";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <AppBar />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/register" element={<RegistrationPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/contacts" element={<ContactsPage />}></Route>
+        </Routes>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
