325e73d85b92deb68956cb3cb19f6bfc283c78c8
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-07-11 18:26:55 +0300

    persistor

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,9 +2,19 @@ import { contactsReducer } from "./contacts/contactsSlice";
 import { combineReducers } from "redux";
 import { filterReducer } from "./filters/filterSlice";
 import { authReducer } from "./auth/slice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["token"],
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
-  auth: authReducer,
+  auth: persistedAuthReducer,
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistStore } from "redux-persist";
 
-export const store = configureStore({ reducer: rootReducer });
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER ],
+      },
+    }),
+});
+
+export const persistor = persistStore(store)
