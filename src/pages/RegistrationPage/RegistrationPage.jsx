177ff5ade37f8f0d3e423a57e1592423d63e0117
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-07-12 18:07:48 +0300

    css

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 import { register } from "../../redux/auth/operations";
+import css from './RegistrationPage.module.css'
 const RegistrationPage = () => {
   const dispatch = useDispatch();
   const handleSubmit = (userData) => {
@@ -8,7 +9,7 @@ const RegistrationPage = () => {
   };
   return (
     <div>
-      <h2>Registration Page</h2>
+      <h2 className={css.header}>Registration Page</h2>
       <RegistrationForm submit={handleSubmit} />
     </div>
   );
