d2260403033a898232861a895651d4b61ffca475
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-07-12 19:24:55 +0300

    css

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,13 @@
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/auth/operations";
-
+import css from "./UserMenu.module.css";
 const UserMenu = () => {
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(logout());
   };
   return (
-    <div>
+    <div className={css.container}>
       <h3>hello, u r in</h3>
       <button onClick={handleLogout}>logout</button>
     </div>
