d57b0798708950927cd0651d0c9a52f6ea7420ab
Your Name <e.scaroline02@gmail.com>
2024-07-14 18:20:35 +0300

    add name in usermenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,14 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/auth/operations";
 import css from "./UserMenu.module.css";
+import { selectUser } from "../../redux/auth/selectors";
 const UserMenu = () => {
+  const {name} = useSelector(selectUser)
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(logout());
   };
   return (
     <div className={css.container}>
-      <h3>hello, u r in</h3>
+      <h3>hello, {name}, u r in</h3>
       <button onClick={handleLogout}>logout</button>
     </div>
   );
